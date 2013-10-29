2dd3d3255a884d0366e94fb949dfcacc4ec65456
Tash <tashage@gmail.com>
2013-10-29 16:04:06 +1100

    Resolution class done, not really tested

--- OpenglTester/OpenglTester/Game1.cs
+++ OpenglTester/OpenglTester/Game1.cs
@@ -16,8 +16,6 @@ namespace OpenglTester
 	/// </summary>
 	public class Game1 : Game
 	{
-		
-
 		public static GraphicsDeviceManager graphics;
 		public static ContentManager contentManager;
 		public static SpriteBatch spriteBatch;
@@ -25,25 +23,25 @@ namespace OpenglTester
 		InputHandler input = new InputHandler();
 		float timeDelta;
 
-
 		public Game1()
 		{
+			//set the screen resolution
 			graphics = new GraphicsDeviceManager(this);
+			Resolution.Init(ref graphics);
+			Resolution.SetBaseResolution(1920, 1080);	 //the resolution that the game is written in
+			Resolution.SetResolution(GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width, GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height, true);	 //the resolution of the actual screen resolution
+			//graphics.IsFullScreen = true; 	//no longer necessary
+
 			contentManager = new ContentManager(Content.ServiceProvider);
-			contentManager.RootDirectory = "Content";	            
+			contentManager.RootDirectory = "Content";	    
 
-
-			graphics.IsFullScreen = true;
 			// Create a new SpriteBatch, which can be used to draw textures.
 			spriteBatch = new SpriteBatch (GraphicsDevice);
-
 
 			// in this example of the animated object:
 			// 4th arg: using the untitled image i only want to render through 3 of the four frames 
 			// 5th arg: time to animate the entire image
 			// size vertically of individual frames
-
-
 		}
 
 		/// <summary>
@@ -55,7 +53,6 @@ namespace OpenglTester
 		protected override void Initialize()
 		{
 			// TODO: Add your initialization logic here
-
 			base.Initialize();
 
 			//initialize the game state manager and set the initial state to the splash screen
@@ -82,28 +79,28 @@ namespace OpenglTester
 		/// <param name="gameTime">Provides a snapshot of timing values.</param>
 		protected override void Update(GameTime gameTime)
 		{
+			//get delta time
 			timeDelta = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+			//update the inputs (keyboard and controller)
 			input.Update();
 
 			//update the current state
 			gameManager.HandleEvents (timeDelta);
 			gameManager.Update (timeDelta);
 
-			// For Mobile devices, this logic will close the Game when the Back button is pressed
 			if(InputHandler.escPressed)
 			{
-				Console.Out.WriteLine("quiting");
+				Console.Out.WriteLine("quitting");
 				Exit ();
 			}
-
+			// For Mobile devices, this logic will close the Game when the Back button is pressed
 			if (GamePad.GetState (PlayerIndex.One).Buttons.Back == ButtonState.Pressed) {
 				Exit ();
 			}
+
 			// TODO: Add your update logic here		
 			base.Update (gameTime);
-
-
-
 		}
 
 		/// <summary>
